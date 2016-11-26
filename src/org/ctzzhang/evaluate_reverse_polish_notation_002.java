d415952f4abb3387a02471a631def10c8935ddff
Czzthang <tzzhang_021@163.com>
2016-11-26 23:17:12 +0800

    list
    evaluate_reverse_polish_notation_002

---
+++ src/org/ctzzhang/evaluate_reverse_polish_notation_002.java
@@ -0,0 +1,55 @@
+package org.ctzzhang;
+
+import java.util.Stack;
+
+/*["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6*/
+
+public class evaluate_reverse_polish_notation_002 {
+	public class Solution {
+		public int evalRPN(String[] tokens) {
+			int num = 0;
+
+			Stack<Integer> s = new Stack<Integer>();
+
+			int len = tokens.length;
+			for (int i = 0; i < len; i++) {
+				if (tokens[i] == "+") {
+					int first=s.pop();
+					int second=s.pop();
+					int sum=first+second;
+					s.add(sum);
+
+				} else if (tokens[i] == "-") {
+					int first=s.pop();
+					int second=s.pop();
+					int sum=second-first;
+					s.add(sum);
+
+				} else if (tokens[i] == "*") {
+					int first=s.pop();
+					int second=s.pop();
+					int sum=first*second;
+					s.add(sum);
+
+				} else if (tokens[i] == "/") {
+					int first=s.pop();
+					int second=s.pop();
+					if(second==0){
+						s.add(second);
+					}else{
+						int m=(int)second/first;
+						s.add(m);
+					
+					}
+
+				} else {
+					s.add(Integer.parseInt(tokens[i]));
+				}
+			}
+			num=s.peek();
+			return num;
+		}
+	}
+
+}
